7f67de8d5c421d1cef3eca49816adcaae2474eb8
danielnandrade <80843426+danielnandrade@users.noreply.github.com>
2021-05-06 14:08:24 +0200

    Update endprojekt_final_fao_analyse.py

--- endprojekt_final_fao_analyse.py
+++ endprojekt_final_fao_analyse.py
@@ -1,9 +1,9 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.formula.api import ols
-import streamlit as st
 from pymongo import MongoClient
 import json
 import os, sys
